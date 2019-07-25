07c2b15a4894f8544e7e617ce00d460c142f6afc
Kumar Gaurav <kumargaurav.cdac@gmail.com>
2019-07-25 12:57:47 -0700

    first commit

---
+++ spring-janus/src/main/java/com/com/kumargaurav/janusgraph/SpringJanusApplication.java
@@ -0,0 +1,13 @@
+package com.com.kumargaurav.janusgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJanusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJanusApplication.class, args);
+	}
+
+}
